9001658d0c32eb55c9a692c8ae14ff60b06df337
Alexander Waite <a.waite1703@gmail.com>
2025-12-03 03:40:31 -0500

    Exerise 4: created a table instead of listing results. Showed country name, independence year, head of state and continent

--- world.php
+++ world.php
@@ -11,8 +11,21 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'; ");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table border= "2">
+    <tr>
+        <th>Country Name</th>
+        <th>Continent</th>
+        <th>Independence Year</th>
+        <th>Head of State</th>
+    </tr>
+    <?php foreach ($results as $row): ?>
+    <tr>
+        <td><?php echo htmlspecialchars($row['name']); ?></td>
+        <td><?php echo htmlspecialchars($row['continent']); ?></td>
+        <td><?php echo htmlspecialchars($row['independence_year']); ?></td>
+        <td><?php echo htmlspecialchars($row['head_of_state']); ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+
