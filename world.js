9740df99e9a4205ef8a975b0a7649eaff8f1a238
Alexander Waite <a.waite1703@gmail.com>
2025-12-03 03:03:42 -0500

    Exerise 3: created and added functionality to JS, modified php

---
+++ world.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const lookupButton = document.getElementById('lookup');
+    lookupButton.addEventListener('click', function() {
+        const countryInput = document.getElementById('country');
+        const country = countryInput.value;
+        const ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                const resultsDiv = document.getElementById('result');
+                resultsDiv.innerHTML = this.responseText;
+                }
+            }
+        ajax.open("GET", "world.php?country=" + encodeURIComponent(country), true);
+        ajax.send();
+    });
+});
